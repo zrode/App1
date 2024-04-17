6d58cdb2f8a2927510f8824275b5df991bbdf74d
Rabah <rabah_zeghoudi@yahoo.fr>
2024-04-17 23:20:32 +0200

    5

--- src/main/java/org/presentation/Presentation.java
+++ src/main/java/org/presentation/Presentation.java
@@ -1,9 +1,15 @@
 package org.presentation;
 
+import org.dao.DaoImpl;
+import org.dao.IDao;
 import org.metier.MetierImpl;
 
 public class Presentation {
     public static void main(String[] args) {
+        MetierImpl metier = new MetierImpl();
+        IDao dao = new DaoImpl();
+        metier.setDao(dao);
+        System.out.println(metier.calcul());
     }
 
 }
